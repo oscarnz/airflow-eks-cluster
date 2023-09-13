1aa35e6da2d9785a9b538a9028daa89690386310
Ariff Nazhan <ariff-zoala@bitbucket.org>
2023-09-13 21:20:05 +0800

    revert

--- eks/main.tf
+++ eks/main.tf
@@ -167,7 +167,6 @@ resource "aws_eks_addon" "csi_driver" {
   cluster_name             = aws_eks_cluster.test.name
   addon_name               = "aws-ebs-csi-driver"
   service_account_role_arn = aws_iam_role.ebs_cni.arn
-  resolve_conflicts_on_update = "PRESERVE"
 }
 
 resource "aws_acm_certificate" "cert" {
