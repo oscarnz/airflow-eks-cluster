f296ec7af116211dc60e70f71330bfe450ed60d1
Ariff Nazhan <ariff-zoala@bitbucket.org>
2023-09-13 21:01:57 +0800

    attach ebs

--- eks/main.tf
+++ eks/main.tf
@@ -70,7 +70,6 @@ resource "aws_eks_cluster" "test" {
   }
 }
 
-
 resource "aws_iam_role" "nodegroup" {
   name = "eks-nodegroup"
 
@@ -115,6 +114,59 @@ resource "aws_eks_node_group" "node1" {
 
   capacity_type = "SPOT"
 
+}
+
+resource "aws_iam_openid_connect_provider" "this" {
+  client_id_list = ["sts.amazonaws.com"]
+  # https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_providers_create_oidc_verify-thumbprint.html
+  # https://github.com/terraform-providers/terraform-provider-tls/issues/52
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+  url             = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
+}
+
+data "aws_iam_policy_document" "ebs_cni_controller" {
+  statement {
+    sid = "EBSCNIAssumeRole"
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity",
+    ]
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.this.arn]
+      type        = "Federated"
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.this.url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.this.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ebs_cni" {
+  name               = "AmazonEKS_EBS_CSI_DriverRole_Data"
+  assume_role_policy = data.aws_iam_policy_document.ebs_cni_controller.json
+
+  tags = module.main.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_cni.name
+}
+
+resource "aws_eks_addon" "csi_driver" {
+  cluster_name             = aws_eks_cluster.cluster.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_cni.arn
 }
 
 resource "aws_acm_certificate" "cert" {
